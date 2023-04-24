6368fc7c1c68fedf2e083a16b9c5cda1006642a6
ukalto <maxi.gaber@gmail.com>
2023-04-24 17:46:26 +0200

    added finished Read and Write Quorums

---
+++ ReadWriteQuorums.py
@@ -0,0 +1,10 @@
+import random
+
+N = int(input("N: "))
+read_important = True if int(input("1 = True : 2 = False ")) == 1 else False
+if read_important:
+    print(f"N_r = {1}\nN_w = {N}")
+else:
+    x = random.randint(N // 2, N)
+    y = random.randint(N - x + 1, N)
+    print(f"N_r = {y}\nN_w = {x}")
