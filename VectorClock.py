cb481bf908af3f82435308a66af11f1dc030eccb
ukalto <maxi.gaber@gmail.com>
2023-04-24 18:15:01 +0200

    added finished VectorClock

---
+++ VectorClock.py
@@ -0,0 +1,29 @@
+# change to your amount
+vector_clocks_amount = 3
+# change to your start vector values
+vectors = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+# message = from to | event(increment) = x
+# Example: task_list = [["13", "2"], ["32"], ["21", "3"], ["31"]]
+task_list = [[]]
+print(f"t0 {vectors}")
+
+
+def update_vectors(vector_from, vector_to):
+    updated_vector = vector_to
+    for i, (f, t) in enumerate(zip(vector_from, vector_to)):
+        if f > t:
+            updated_vector[i] = f
+    return updated_vector
+
+
+for i, tasks in enumerate(task_list):
+    for task in tasks:
+        if len(task) == 2:
+            f, t = int(task[0]) - 1, int(task[1]) - 1
+            vectors[f][f] += 1
+            vectors[t] = update_vectors(vectors[f], vectors[t])
+            vectors[t][t] += 1
+        else:
+            inc = int(task[0])
+            vectors[inc - 1][inc - 1] += 1
+    print(f"t{i + 1} {vectors}")
