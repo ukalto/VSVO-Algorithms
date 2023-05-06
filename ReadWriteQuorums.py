72153d04851093d8117a4db8c1360cfc3197e328
ukalto <maxi.gaber@gmail.com>
2023-05-07 00:17:59 +0200

    adjusted input case for False in if statement

--- ReadWriteQuorums.py
+++ ReadWriteQuorums.py
@@ -1,7 +1,7 @@
 import random
 
 N = int(input("N: "))
-read_important = True if int(input("1 = True : 2 = False ")) == 1 else False
+read_important = True if int(input("1 = True : Other = False ")) == 1 else False
 if read_important:
     print(f"N_r = {1}\nN_w = {N}")
 else:
